3020b0d3fc6b997b3b130d10a1209ce59e5cc74c
1JeongYeon <102153402+1JeongYeon@users.noreply.github.com>
2022-07-05 22:15:29 +0900

    animation
    캐릭터에 애니메이션 추가함 스크립트 살짝 수정

--- RunningGame/Assets/Scripts/Movement/PlayerMove.cs
+++ RunningGame/Assets/Scripts/Movement/PlayerMove.cs
@@ -4,47 +4,73 @@ using UnityEngine;
 
 public class PlayerMove: MonoBehaviour
 {
+    public Animator animator;
+    RuntimeAnimatorController controller;
+
     [SerializeField] private float moveSpeed;
     [SerializeField] private float boostMultiplier;
     [SerializeField] private float jumpHeight;
 
     private Rigidbody2D rigid;
-    private int jumpCount = 0;
+    private int jumpCount = 2;
+    private bool isJump = false;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+       
     }   
 
     void Update()
     {
         PlayerControl();
-
+        
         this.transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
-        {
-            jumpCount = 0;
-        }
-    }
+    
 
     private void PlayerControl()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+       
+
+        if (jumpCount > 0)
         {
-            if (jumpCount < 2)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                rigid.velocity = new Vector2(0, jumpHeight);
-                jumpCount += 1;
+                if (!isJump)
+                {
+                    isJump = true;
+                    rigid.AddForce(Vector3.up * jumpHeight, ForceMode2D.Impulse);
+                    animator.SetBool("jump", true);
+                    jumpCount--;
+
+                }
+                else if (jumpCount == 1) // 더블점프
+                {
+                    animator.SetBool("jump", false);
+                    animator.SetBool("doublejump", true);
+                    rigid.AddForce(Vector3.up * (jumpHeight / 1.5f), ForceMode2D.Impulse);
+                    isJump = false;
+                    jumpCount--;
+                }
             }
         }
+
 
         if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
         {
 
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer.Equals(LayerMask.NameToLayer("Ground")))
+        {
+            isJump = false;
+            animator.SetBool("jump", false);
+            animator.SetBool("doublejump", false);
+            jumpCount = 2;
         }
     }
 }
